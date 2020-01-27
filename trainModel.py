16ca56b2b2d9dc91c8acee5236d09b15ed42082d
Brian Chen <brian.chen@queensu.ca>
2020-01-27 13:58:18 -0500

    Add missing file and update model training to work with any path

--- trainModel.py
+++ trainModel.py
@@ -1,9 +1,8 @@
 import numpy
 import math
-import keras.models
 from keras import optimizers
 from keras.utils import to_categorical,Sequence
-from keras.model import model_from_json
+from keras.models import model_from_json
 
 from models import unet3D
 
@@ -12,85 +11,95 @@ import os
 
 from generateDataSetSequence import LNDbSequence
 
-class trainModelUtilities():
+from argparse import ArgumentParser
 
-    def createModel():
-        # author Rebecca Hisey
-        imageSize = 64
-        bottleneck_size = imageSize/4
-        numFilters = 6
-        regularizer = 0
-        dropoutRate = 0.1
-        seed = 0
-        batchNormalization = 1
+def createModel():
+    # author Rebecca Hisey
+    imageSize = 64
+    bottleneck_size = imageSize/4
+    numFilters = 6
+    regularizer = 0
+    dropoutRate = 0.1
+    seed = 0
+    batchNormalization = 1
 
-        unet = unet3D(imageSize,bottleneck_size,numFilters,regularizer,dropoutRate,seed,batchNormalization)
-        unetModel = unet.createModel()
-        return unetModel
+    unet = unet3D(imageSize,bottleneck_size,numFilters,regularizer,dropoutRate,seed,batchNormalization)
+    unetModel = unet.createModel()
+    return unetModel
 
-    def trainModel(unetModel,dataFolder):
-        # author Rebecca Hisey
-        sgd = optimizers.SGD()
-        (images, segmentations) = loadSamples(dataFolder)
-        datasetSequence = LNDbSequence(images,segmentations,batchSize=10)
-        unetModel.compile(optimizer=sgd, loss='mean_squared_error')
-        unetModel.fit(x=datasetSequence.inputs,y=datasetSequence.targets,batch_size=10)
-        return unetModel
+def trainModel(unetModel,dataFolder):
+    # author Rebecca Hisey
+    sgd = optimizers.SGD()
+    (images, segmentations) = loadSamples(dataFolder)
+    datasetSequence = LNDbSequence(images,segmentations,batchSize=10)
+    unetModel.compile(optimizer=sgd, loss='mean_squared_error')
+    unetModel.fit(x=datasetSequence.inputs,y=datasetSequence.targets,batch_size=10)
+    return unetModel
 
-    def saveModel(model,filePath):
-        # author Rebecca Hisey
-        JSONmodel = model.to_json()
-        structureFileName = 'lungSegmentationUNet.json'
-        weightsFileName = 'lungSegmentationUNet.h5'
-        with open(os.path.join(filePath,structureFileName),"w") as modelStructureFile:
-            modelStructureFile.write(JSONmodel)
-        model.save_weights(os.path.join(filePath,weightsFileName))
+def saveModel(model,filePath):
+    # author Rebecca Hisey
+    JSONmodel = model.to_json()
+    structureFileName = 'lungSegmentationUNet.json'
+    weightsFileName = 'lungSegmentationUNet.h5'
+    with open(os.path.join(filePath,structureFileName),"w") as modelStructureFile:
+        modelStructureFile.write(JSONmodel)
+    model.save_weights(os.path.join(filePath,weightsFileName))
 
-    def loadModel(filePath):
-        # author Rebecca Hisey
-        structureFileName = 'lungSegmentationUNet.json'
-        weightsFileName = 'lungSegmentationUNet.h5'
-        with open(os.path.join(filePath,structureFileName),"r") as modelStructureFile:
-            JSONModel = modelStructureFile.read()
-        model = model_from_json(JSONModel)
-        model.load_weights(os.path.join(filePath,weightsFileName))
-        return model
+def loadModel(filePath):
+    # author Rebecca Hisey
+    structureFileName = 'lungSegmentationUNet.json'
+    weightsFileName = 'lungSegmentationUNet.h5'
+    with open(os.path.join(filePath,structureFileName),"r") as modelStructureFile:
+        JSONModel = modelStructureFile.read()
+    model = model_from_json(JSONModel)
+    model.load_weights(os.path.join(filePath,weightsFileName))
+    return model
 
-    def preprocess(image):
-        #author Rebecca Hisey
-        image = numpy.load(image)
-        image = normalize(image)
-        return image
+def preprocess(image):
+    #author Rebecca Hisey
+    image = numpy.load(image)
+    image = normalize(image)
+    return image
 
-    def normalize(image):
-        #author Rebecca Hisey
-        normImage = (image - numpy.min(image))/(numpy.max(image) - numpy.min(image))
-        normImage = numpy.expand_dims(normImage, axis=0)
-        normImage = numpy.expand_dims(normImage, axis=-1)
-        return normImage
+def normalize(image):
+    #author Rebecca Hisey
+    normImage = (image - numpy.min(image))/(numpy.max(image) - numpy.min(image))
+    normImage = numpy.expand_dims(normImage, axis=0)
+    normImage = numpy.expand_dims(normImage, axis=-1)
+    return normImage
 
-    def loadSamples(currentDirectory):
-        #author Rebecca Hisey
-        R1Path = os.path.join(currentDirectory,'R1')
-        images = []
-        segmentations = []
-        allFiles = os.listdir(R1Path)
-        for imageNo in range(1,len(allFiles)):
-            fileName = allFiles[imageNo]
-            if 'Mask' in fileName:
-                [_,imageFileName] = fileName.split('_')
-                image = preprocess(os.path.join(R1Path,imageFileName))
+def loadSamples(currentDirectory):
+    #author Rebecca Hisey
+    R1Path = os.path.join(currentDirectory,'R1')
+    images = []
+    segmentations = []
+    allFiles = os.listdir(R1Path)
+    for imageNo in range(1,len(allFiles)):
+        fileName = allFiles[imageNo]
+        if 'Mask' in fileName:
+            [_,imageFileName] = fileName.split('_')
+            image = preprocess(os.path.join(R1Path,imageFileName))
 
-                segmentation = preprocess(os.path.join(R1Path,fileName))
-                images.append(image)
-                segmentations.append(segmentation)
-        return (images,segmentations)
+            segmentation = preprocess(os.path.join(R1Path,fileName))
+            images.append(image)
+            segmentations.append(segmentation)
+    return (images,segmentations)
 
-def main():
+def main(dataDirectory, saveModelDirectory):
     #author Rebecca Hisey
     unet = createModel()
-    unet = trainModel(unet,'c:/Users/hisey/Documents/CISC_881/Challenge/Nodules/')
-    saveModel(unet,'c:/Users/hisey/Documents/CISC_881/Challenge/')
+# 'c:/Users/hisey/Documents/CISC_881/Challenge/Nodules/'
+    unet = trainModel(unet, dataDirectory)
+# 'c:/Users/hisey/Documents/CISC_881/Challenge/'
+    saveModel(unet, saveModelDirectory)
 
-main()
 
+if __name__ == '__main__':
+    currentDirectory = os.getcwd()
+
+    parser = ArgumentParser(description='Trains segmentation model')
+    parser.add_argument('-d', '--data', default=os.path.join(currentDirectory, 'Data'))
+    parser.add_argument('-s', '--save', default=currentDirectory)
+
+    args = parser.parse_args()
+    main(args.data, args.save)

c267273fb978e951840c75c0d62084bd03fbfe9b
Sindhura Thiru <sindhurathiru@gmail.com>
2020-02-07 11:54:40 -0500

    ENH: Function calculates volume given image and segmentation

--- segmentationVolume.py
+++ segmentationVolume.py
@@ -4,5 +4,12 @@ import os
 
 def segVol(img, segmentation):
     """
-    Calculate the volume of segmentation, given the original image
+    Author: Sindhura Thirumal
+    Takes in an image and its segmentation and returns the volume of the segmentation
     """
+    imgArray = sitk.GetArrayFromImage(img)
+    maskArray = sitk.GetArrayFromImage(segmentation)
+    spacing = img.GetSpacing()
+    pixelCount = np.count_nonzero(maskArray)
+    volume = pixelCount * spacing[0] * spacing[1] * spacing[2]
+    return volume

ba4e7c51c758e984f1a68feef743f12d2c2f0ee9
Sindhura Thiru <sindhurathiru@gmail.com>
2020-02-07 11:56:08 -0500

    Update segmentationVolume.py

--- segmentationVolume.py
+++ segmentationVolume.py
@@ -7,7 +7,6 @@ def segVol(img, segmentation):
     Author: Sindhura Thirumal
     Takes in an image and its segmentation and returns the volume of the segmentation
     """
-    imgArray = sitk.GetArrayFromImage(img)
     maskArray = sitk.GetArrayFromImage(segmentation)
     spacing = img.GetSpacing()
     pixelCount = np.count_nonzero(maskArray)

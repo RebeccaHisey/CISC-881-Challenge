ff86b777402dd5b969a23f12a197a874c9f37c59
Sindhura Thiru <sindhurathiru@gmail.com>
2020-02-07 14:06:37 -0500

    ENH: findCentroidNodule only keeps the centroid nodule in the output, deletes other nodules in cube

--- thresholdOutput.py
+++ thresholdOutput.py
@@ -7,14 +7,24 @@ def thresholdCube(outputArray):
     Takes in array outputted by trainModel and thresholds to create a binary image.
     Output is a label array of each of the nodules in the cube
     """
-    # author Sindhura Thirumal
-    # Scale image to be in range 0-255
-    outputArray *= 255.0/outputArray.max()
-    # Convert data type of array to uint8
-    intImg = outputArray.astype(np.uint8)
+    outputArray *= 255.0/outputArray.max()  # Scale image to be in range 0-255
+    intImg = outputArray.astype(np.uint8)   # Convert data type of array to uint8
     for i in range(intImg.shape[2]):
         otsuThreshVal = mh.otsu(intImg[i])
         intImg[i] = intImg[i] > otsuThreshVal
         intImg[i], nNodules = mh.label(intImg[i])
     return intImg
 
+def findCentroidNodule(labeledSeg):
+    """
+    Author: Sindhura Thirumal
+    Given the labeled array of the segmentation, returns the nodule segmentation that overlaps with
+    the centroid. Deletes all other nodules within cube.
+    """
+    centroid = [40, 40, 40]
+    centroidSeg = labeledSeg[centroid]
+    for i in range(labeledSeg.shape[2]):
+        nNods = np.unique(labeledSeg[i])
+        for j in range(len(nNods)):
+            labeledSeg[i][labeledSeg[i] == j] = 0
+    return labeledSeg

c063fd15144bfe3bedd9a5f83a9531938f80d1d3
Sindhura Thiru <sindhurathiru@gmail.com>
2020-02-07 14:12:41 -0500

    STYLE: Added comments

--- thresholdOutput.py
+++ thresholdOutput.py
@@ -10,22 +10,23 @@ def thresholdCube(outputArray):
     outputArray *= 255.0/outputArray.max()  # Scale image to be in range 0-255
     intImg = outputArray.astype(np.uint8)   # Convert data type of array to uint8
     for i in range(intImg.shape[2]):
-        otsuThreshVal = mh.otsu(intImg[i])
+        otsuThreshVal = mh.otsu(intImg[i])  # Otsu thresholding
         intImg[i] = intImg[i] > otsuThreshVal
-        intImg[i], nNodules = mh.label(intImg[i])
+        intImg[i], nNodules = mh.label(intImg[i])   # Label the segmentation based on nodule
     return intImg
 
 def findCentroidNodule(labeledSeg):
     """
     Author: Sindhura Thirumal
-    Given the labeled array of the segmentation, returns the nodule segmentation that overlaps with
-    the centroid. Deletes all other nodules within cube.
+    Given the labeled array of the segmentation (outputted from thresholdCube()), finds the nodule segmentation that
+    overlaps with the centroid. Deletes all other nodules within cube. Output is the resulting binary image, only containing
+    the centroid nodule.
     """
     centroid = [40, 40, 40]
-    centroidSeg = labeledSeg[centroid]
+    centroidSeg = labeledSeg[centroid]  # Get label of the nodule overlapping the centroid
     for i in range(labeledSeg.shape[2]):
-        nNods = np.unique(labeledSeg[i])
+        nNods = np.unique(labeledSeg[i])    # Get number of total nodules in segmentation
         for j in range(len(nNods)):
             if j != centroidSeg:
-                labeledSeg[i][labeledSeg[i] == j] = 0
+                labeledSeg[i][labeledSeg[i] == j] = 0   # If nodule does not belong to centroid nodule, delete it
     return labeledSeg
